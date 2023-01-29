c0fddef0ac3a2cb34801283165ef2a78af278b1b
Durgesh Mehar <durgeshmehar1380@gmail.com>
2023-01-28 16:10:15 -0800

    Rendering Expenses with performance optimisations

--- src/App.js
+++ src/App.js
@@ -1,35 +1,32 @@
-import React from 'react';
+import React, { useState } from "react";
+import NewExpenese from "./components/NewExpense/NewExpense";
+import Expenses from "./components/Expenses/Expenses";
 
-import NewExpense from './components/NewExpense/NewExpense';
-import Expenses from './components/Expenses/Expenses';
+const DUMMY_EXPENSE = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
 
 const App = () => {
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Toilet Paper',
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
-
-  const addExpenseHandler = expense => {
-    console.log('In App.js');
-    console.log(expense);
-  };
+  
+  const [expenses,setExpense] = useState(DUMMY_EXPENSE)
 
   // return React.createElement(
   //   'div',
@@ -38,12 +35,16 @@ const App = () => {
   //   React.createElement(Expenses, { items: expenses })
   // );
 
+  const addExpenseHandler = (expense) => {
+    const updatedExpense = [expense, ...expenses];
+    setExpense(updatedExpense);
+  };
   return (
     <div>
-      <NewExpense onAddExpense={addExpenseHandler} />
+      <NewExpenese onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   );
-}
+};
 
 export default App;
